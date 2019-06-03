622b0be6cf12625f41ec0e369572317a3f29b068
JOFLIX <joflixooko@outlook.com>
2019-06-04 00:47:24 +0300

    consoled|

--- script/script.js
+++ script/script.js
@@ -7,24 +7,31 @@ function calculator(){
   // #my business logic/
   if(a===b && b===c && c===a){
            alert("equilateral Triangle");
+           console.log('congrats! keep trying')
   }
   else if(a==b && a + b > c || b==c && b + c > a || a== c && a + c >b){
           alert("isosceles triangle");
+          console.log('congrats! keep trying')
   }
   else if(a!==b && b!==c && c!==a && a + c>b && b + c >a && a+b>c){
           alert("scalene triangle");
+          console.log('congrats! keep trying')
   }
   else if (a + b <= c ){
           alert("not a triangle");
+          console.log('congrats! keep trying')
   }
   else if(b + c <= a ){
     alert("not a triangle");
+    console.log('congrats! keep trying')
   }
   else if(a + c <= b){
     alert("not a triangle");
+    console.log('congrats! keep trying')
   }
   
   else (alert("input a valid integer"))
+  console.log('Oops! keep trying')
   
   }
   
